3753ba6a5050a3e16de4763070888e99d84e64a3
Viktor <viktornord1@rambler.ru>
2013-01-15 21:30:00 +0200

    Community A+V

--- templates/personality/index.php
+++ templates/personality/index.php
@@ -22,8 +22,6 @@ $app = JFactory::getApplication();
      <!--FONT-->
     <link href='http://fonts.googleapis.com/css?family=PT+Sans' rel='stylesheet' type='text/css'>
     <!-- The following five lines load the Blueprint CSS Framework (http://blueprintcss.org). If you don't want to use this framework, delete these lines. -->
-    <link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/<?php echo $this->template ?>/css/blueprint/screen.css" type="text/css" media="screen, projection" />
-    <link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/<?php echo $this->template ?>/css/blueprint/print.css" type="text/css" media="print" />
     <link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/<?php echo $this->template ?>/css/template.css" type="text/css" />
     <!--[if lt IE 8]><link rel="stylesheet" href="blueprint/ie.css" type="text/css" media="screen, projection"><![endif]-->
     <link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/<?php echo $this->template ?>/css/blueprint/plugins/fancy-type/screen.css" type="text/css" media="screen, projection" />
@@ -33,60 +31,88 @@ $app = JFactory::getApplication();
 		<!-- The following line loads the template JavaScript file located in the template folder. It's blank by default. -->
 		<script type="text/javascript" src="<?php echo $this->baseurl ?>/templates/<?php echo $this->template ?>/js/template.js"></script>
 
-        <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.4.3/jquery.min.js"></script>
+    <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.4.3/jquery.min.js"></script>
 
-        <script type="text/javascript">
-            $(document).ready(function(){
+    <script type="text/javascript">
+        $(document).ready(function(){
 
-                $(".trigger").click(function(){
-                    $(".panel").toggle("slow");
-                    $(this).toggleClass("active");
-                    return false;
-                });
+            $(".trigger").click(function(){
+                $(".panel").toggle("slow");
+                $(this).toggleClass("active");
+                return false;
             });
-        </script>
+        });
+    </script>
 
 
 
-        <!-- Reading browser's window width & height and applying them for sheduler -->
-        <script type="text/javascript">
-            $(document).ready(function(){
+    <!-- Reading browser's window width & height and applying them for sheduler -->
+    <script type="text/javascript">
+        $(document).ready(function(){
+            $(".panel").css({'width':$(window).width(), 'height':$(window).height()});
+            $(".pwrap").css('right',function(){return (($(window).width()-920)/2)});
+            if ($(window).height()>'700'){$(".pwrap").css('top',function(){return (($(window).height()-700)/2)})};
+            window.onresize = function() {
                 $(".panel").css({'width':$(window).width(), 'height':$(window).height()});
                 $(".pwrap").css('right',function(){return (($(window).width()-920)/2)});
                 if ($(window).height()>'700'){$(".pwrap").css('top',function(){return (($(window).height()-700)/2)})};
-                window.onresize = function() {
-                    $(".panel").css({'width':$(window).width(), 'height':$(window).height()});
-                    $(".pwrap").css('right',function(){return (($(window).width()-920)/2)});
-                    if ($(window).height()>'700'){$(".pwrap").css('top',function(){return (($(window).height()-700)/2)})};
-                };
-                return false;
-            });
-        </script>
-        <!-- Reading browser's window width & height and applying them for sheduler -->
-
+            };
+            return false;
+        });
+    </script>
+    <!-- Reading browser's window width & height and applying them for sheduler -->
     </head>
 	<body>
-		<div class="container">
-	<div class="header">
-        <div class="wrap_logo">
-            <a href="index.php/news">
-                <h1>Personality</h1>
-                <span>LOGO</span>
-            </a>
+	<div class="container">
+        <div class="header">
+            <div class="wrap_logo">
+                <a href="index.php/news">
+                    <h1>Personality</h1>
+                    <span>LOGO</span>
+                </a>
+            </div>
+            <jdoc:include type="modules" name="personality-topmenu" />
         </div>
-        <jdoc:include type="modules" name="personality-topmenu" />
-    </div>
-    <div class="content">
-        <jdoc:include type="modules" name="before-content" />
-        <jdoc:include type="component" />
-        <jdoc:include type="modules" name="personality-map" />
-        <jdoc:include type="modules" name="personality-lightbox" />
-    </div>
+        <div class="content">
+            <jdoc:include type="modules" name="before-content" />
+            <jdoc:include type="component" />
+            <jdoc:include type="modules" name="personality-map" />
+            <jdoc:include type="modules" name="personality-lightbox" />
+        </div>
+        <div class="panel">
+            <div class="bg"></div>
+            <div class="pwrap">
+
+                <h4>Розклад занять</h4>
+                <ul>
+                    <li>
+                        <jdoc:include type="modules" name="Rozklad_pn" />
+                    </li>
+
+                    <li class="blue">
+                        <jdoc:include type="modules" name="Rozklad_vt" />
+                    </li>
+
+                    <li class="red">
+                        <jdoc:include type="modules" name="Rozklad_sr" />
+                    </li>
+
+                    <li class="blue panel_new_row">
+                        <jdoc:include type="modules" name="Rozklad_4t" />
+                    </li>
+                    <li>
+                        <jdoc:include type="modules" name="Rozklad_pt" />
+                    </li>
+                </ul>
+            </div>
+            <a class="trigger" href="#"></a>
+        </div>
+        <a class="trigger" href="#"></a>
 
             <!--SHEDULER SHEDULER SHEDULER--><!--SHEDULER SHEDULER SHEDULER--><!--SHEDULER SHEDULER SHEDULER-->
             <!--SHEDULER SHEDULER SHEDULER--><!--SHEDULER SHEDULER SHEDULER--><!--SHEDULER SHEDULER SHEDULER-->
             <!--SHEDULER SHEDULER SHEDULER--><!--SHEDULER SHEDULER SHEDULER--><!--SHEDULER SHEDULER SHEDULER-->
-            <div class="panel">
+          <!--  <div class="panel">
                 <div class="bg"></div>
                 <div class="pwrap">
 
@@ -121,6 +147,6 @@ $app = JFactory::getApplication();
             <!--END SHEDULER END SHEDULER END SHEDULER--><!--END SHEDULER END SHEDULER END SHEDULER-->
             <!--END SHEDULER END SHEDULER END SHEDULER--><!--END SHEDULER END SHEDULER END SHEDULER-->
 
-        </div>
+    </div>
 	</body>
 </html>
