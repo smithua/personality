981df2012a09c64684d5f7f474afcb5aea3996fd
Viktor <viktornord1@rambler.ru>
2013-01-20 20:08:00 +0200

    placeholder

---
+++ templates/personality/js/placeholder.js
@@ -0,0 +1,19 @@
+$(function() {
+    var active = document.activeElement;
+    $(':text, textarea').focus(function () {
+        if ($(this).attr('placeholder') != '' && $(this).val() == $(this).attr('placeholder')) {
+            $(this).val('').removeClass('hasPlaceholder');
+        }
+    }).blur(function () {
+            if ($(this).attr('placeholder') != '' && ($(this).val() == '' || $(this).val() == $(this).attr('placeholder'))) {
+                $(this).val($(this).attr('placeholder')).addClass('hasPlaceholder');
+            }
+        });
+    $(':text, textarea').blur();
+    $(active).focus();
+    $('form').submit(function () {
+        $(this).find('.hasPlaceholder').each(function () {
+            $(this).val('');
+        });
+    });
+});
